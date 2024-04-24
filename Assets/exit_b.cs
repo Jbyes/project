d522ec90572c834e1a4a53bfcddba67c74cc2a6f
Jbyes <105591757+Jbyes@users.noreply.github.com>
2024-04-24 13:18:07 +0900

    ui

---
+++ Assets/exit_b.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit_b : MonoBehaviour
+{
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
